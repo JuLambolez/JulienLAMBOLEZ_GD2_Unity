f9c07c9168bca066e647cb927f1b20c3684e69e5
JuLambolez <julienlambolez@gmail.com>
2025-09-16 18:06:18 +0200

    Création de cibles, de points et de materials
    Création de cibles ainsi que la liaison avec un mini système de scoring via ScoreData. Création de quelques materials simple

---
+++ Assets/Scripts/Target_Soft.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Target_Soft : MonoBehaviour
+{
+    [SerializeField] private int _targetValue = -1;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Player_Collect>() != null)
+        {
+            other.gameObject.GetComponent<Player_Collect>().UpdateScore(_targetValue);
+            Destroy(gameObject);
+        }
+    }
+}

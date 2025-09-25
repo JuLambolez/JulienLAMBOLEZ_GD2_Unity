5fc9e7081491db243a9fc21921a56a0789d74ac7
JuLambolez <julienlambolez@gmail.com>
2025-09-25 13:57:28 +0200

    ajout d'un UI score sur l'écran

--- Assets/Scripts/Target_Soft.cs
+++ Assets/Scripts/Target_Soft.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Target_Soft : MonoBehaviour
 {
-    [SerializeField] private int _targetValue = -1;
+    [SerializeField] private int _targetValue = (-1);
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<Player_Collect>() != null)
